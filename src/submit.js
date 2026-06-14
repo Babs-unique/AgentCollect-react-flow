13be9025dac5612712144f73baabdececbaf35c6
Babatunde Adegboye <babatundeadegboye2008@gmail.com>
2026-06-14 18:53:01 +0100

    Added submit duntion

--- src/submit.js
+++ src/submit.js
@@ -1,16 +1,32 @@
 // submit.js
 import './styles/submit.css'
-import 
 import { useStore } from './store';
 export const SubmitButton = () => {
-    console.log(useStore(state => state.nodes))
-    console.log(useStore(state => state.edges))
+    const nodes = useStore(state => state.nodes)
+    const edges = useStore(state => state.edges)
+
+    const handleSubmit = async () =>{
+        const response = await fetch('http://localhost:8000/pipeline', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ nodes, edges }),
+        });
+        try {
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
             <button
                     type="submit"
                     className='submit-btn'
+                    onClick={handleSubmit}
                     >
                 Analyze Pipeline
             </button>
